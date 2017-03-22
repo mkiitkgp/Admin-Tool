5ffbd2967772279279bed70a25a23be274b2eb4a
Mohit <mohit@stayzilla.com>
2017-03-22 16:07:12 +0530

    Adding new Feature

--- index.js
+++ index.js
@@ -6,11 +6,54 @@ MyNameSpace.helpers = {
 var app = angular.module('BlankApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages','moment-picker']);
 
 
-app.controller('myController', function ($scope,$mdDialog, $http) {
+app.controller('myController', function ($scope,$mdDialog, $http , $rootScope) {
 
 	$scope.adminsName = ["Haardik" , "Lather","Ankit","Vishal"];
 
 	$scope.subjectNameArray = ["Dynamics","Material Science" ,"Manufacturing","MOM","Organic Chemistry","Measurements","Engineering Eco","Control System"];
+	$scope.examType = ["Exam","Quiz","Final Exam","Pop Quiz"];
+
+	$rootScope.reportingLiveCheckBox = false;
+	$rootScope.HomeworkCheckBox = false;
+	$rootScope.numberOfReviewsTextBox = 0;
+	$scope.reviewRequired= [];
+	$scope.radioGroup = true;
+
+    $scope.$watch('radioGroup', function(newVal, oldVal){
+    	
+    		console.log(newVal);
+    		if(newVal == true){
+    			$scope.examType = [];
+    			$scope.examType = ["Exam","Quiz","Final Exam","Pop Quiz"];
+    		}
+    		else{
+    			$scope.examType= [];
+    			$scope.examType = ["Lab","HomeWork","Project"];
+    		}
+	}, true);
+
+	//$scope.numberOfTutor
+
+	$scope.$watch('numberOfTutor', function(newVal, oldVal){
+		if(newVal != undefined){
+    		console.log("change in value " + newVal);  
+    		$rootScope.numberOfReviewsTextBox = newVal;
+    		$scope.numberRev = newVal;
+    	}
+    	else{
+    		console.log("change in value " + newVal); 
+    		$scope.numberRev = null;
+    	}
+	}, true);
+
+	$scope.range = function(n) {
+        return new Array(n);
+    };
+
+    //$scope.reportingLiveCheckBox = $scope.optionsType[0].checked;
+    //$scope.HomeworkCheckBox = $scope.optionsType[1].checked;
+
+   
 
 	$scope.obj={
 		adminName:"",
@@ -74,6 +117,11 @@ app.controller('myController', function ($scope,$mdDialog, $http) {
     $scope.submit = function() {
 
     	$scope.showConfirm();
+    	$scope.obj.numberOfTutor = $scope.numberOfTutor;
+    	$scope.obj.ratingArray =[];
+    	$scope.obj.ratingArray = $scope.reviewRequired;
+    	console.log($scope.obj);
+    	//console.log($scope.reviewRequired);
 
 
     };
